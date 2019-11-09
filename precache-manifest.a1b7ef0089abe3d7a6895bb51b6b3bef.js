d04c8e27d7584e33595e1ffe5ae20e38e66ac209
Eliseo Flores <eliseo_flores530@yahoo.com>
2019-11-08 17:28:28 -0800

    Updates

---
+++ precache-manifest.a1b7ef0089abe3d7a6895bb51b6b3bef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfd82cc0357559e445e598c3a5d1802",
+    "url": "/reactnd-project-readable/frontend/index.html"
+  },
+  {
+    "revision": "fef1f404cac632515687",
+    "url": "/reactnd-project-readable/frontend/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6b901703311042842fe1",
+    "url": "/reactnd-project-readable/frontend/static/js/2.03dc0dca.chunk.js"
+  },
+  {
+    "revision": "fef1f404cac632515687",
+    "url": "/reactnd-project-readable/frontend/static/js/main.63325c77.chunk.js"
+  },
+  {
+    "revision": "d0075cca2d2f27e6543b",
+    "url": "/reactnd-project-readable/frontend/static/js/runtime~main.5c2170f3.js"
+  }
+]);
